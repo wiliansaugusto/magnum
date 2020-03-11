ced86fd8407543fd46792e2554b5ca8fd83e8f46
Willians Augusto <wiliansaugusto@gmail.com>
2020-03-11 05:16:57 -0300

    corrigido as migrations

--- database/migrations/2020_03_11_080851_mgm_tbl_palestrante_contato.php
+++ database/migrations/2020_03_11_080851_mgm_tbl_palestrante_contato.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class MgmTblPalestranteContrato extends Migration
+class MgmTblPalestranteContato extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,15 @@ class MgmTblPalestranteContrato extends Migration
      */
     public function up()
     {
-        Schema::create('mgm_tbl_palestrante_contrato', function (Blueprint $table) {
-            $table->bigIncrements('id_palestrante_contrato');
+        Schema::create('mgm_tbl_palestrante_contato', function (Blueprint $table) {
+
+            $table->bigIncrements('id');
             $table->timestamps();
             $table->unsignedBigInteger('id_palestrante');
             $table->foreign('id_palestrante')->references('id')->on('mgm_tbl_palestrante');
             $table->unsignedBigInteger('id_contato');
             $table->foreign('id_contato')->references('id')->on('mgm_tbl_contato');
+
         });
     }
 
@@ -30,6 +32,6 @@ class MgmTblPalestranteContrato extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('mgm_tbl_palestrante_contrato');
+        Schema::dropIfExists('mgm_tbl_palestrante_contato');
     }
 }
