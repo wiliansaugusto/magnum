d9bc646eba75d46f64e8713f62500115410a27e3
Diego de Lima Lopes <diego.lima.lopes@hotmail.com.br>
2020-03-22 02:08:24 -0300

    Ajustes para teste local

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+//Route::get('/home', 'HomeController@index')->name('home');
 
-Route::resource('/dashboard/palestrante/', 'PalestranteController');
+Route::resource('/', 'PalestranteController');
+//Route::resource('/dashboard/palestrante/', 'PalestranteController');
