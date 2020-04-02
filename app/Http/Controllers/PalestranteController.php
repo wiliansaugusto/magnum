cbf78eabfd33e101bd6352ee9c93dddcd4138a65
Diego de Lima Lopes <diego.lima.lopes@hotmail.com.br>
2020-04-02 15:09:52 -0300

    Correções em paletrantes

--- app/Http/Controllers/PalestranteController.php
+++ app/Http/Controllers/PalestranteController.php
@@ -19,7 +19,7 @@ class PalestranteController extends Controller
      */
     public function index()
     {
-        return view('dashboard.palestrante.create');
+        return view('dashboard.palestrante.index');
     }
 
     /**
--- resources/views/dashboard/palestrante/index.blade.php
+++ resources/views/dashboard/palestrante/index.blade.php
@@ -1,32 +1,29 @@
-@extends('layouts.app')
+@extends('layouts.dashboard')
 
 @section('content')
-<div class="card">
-    <div class="card-header ">
+    <div class="card">
+        <div class="card-header ">
 
-        <div class="row justify-content-center">
-            <div class="col-md-6 sm-12 text-left">
-                <h3>Palestrante</h3>
-            </div>
-            <div class="col-md-6 sm-12 text-right">
-                <div class="form-check form-check-inline">
-                    <button type="button" class="btn btn-primary" data-toggle="modal"
-                        data-target="#frmNomePalestranteModal"><i class="fas fa-plus "></i> Adicionar Palestrante
-                    </button>
+            <div class="row justify-content-center">
+                <div class="col-md-6 sm-12 text-left">
+                    <h3>Palestrante</h3>
+                </div>
+                <div class="col-md-6 sm-12 text-right">
+                    <div class="form-check form-check-inline">
+                        <button type="button" class="btn btn-primary" data-toggle="modal"
+                                data-target="#frmNomePalestranteModal"><i class="fas fa-plus "></i> Adicionar
+                            Palestrante
+                        </button>
+                    </div>
                 </div>
             </div>
+
         </div>
-
-    </div>
-    <div class="card-body">
-        <h5 class="card-title">Titulo do Card</h5>
-        <p class="card-text">Corpo do Card</p>
+        <div class="card-body">
+            <h5 class="card-title">Titulo do Card</h5>
+            <p class="card-text">Corpo do Card</p>
 
 
+        </div>
     </div>
-    <div class="card-footer text-muted text-center">
-        Rodapé do card
-    </div>
-</div>
-@include('dashboard.palestrante.index')
 @endsection
