62984ab9315ac3a4801b4c5144e50e58f7943780
Benício Neto <51092683+liconeto@users.noreply.github.com>
2020-06-09 22:39:37 -0300

    migratios

---
+++ database/migrations/2020_06_09_215019_alterar_foreign_key.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterarForeignKey extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('mgm_tbl_evento', function (Blueprint $table) {
+            //
+            $table->foreign('id_tipo_evento')->references('id')
+            ->on('mgm_tbl_tipo_evento')->onDelete('cascade');
+
+        });
+
+        
+
+
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('mgl_tbl_evento', function (Blueprint $table) {
+            //
+        });
+    }
+}
